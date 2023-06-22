3afa657ce5d8e7fe7ac8280492c66d165e3389fd
nitishseh <136986361+nitishseh@users.noreply.github.com>
2023-06-22 21:34:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
-API_ID       = 12124605
-API_HASH     = "5cf3577d85fd02286535ec2296934287"
+API_ID       = 26374756
+API_HASH     = "2e3be9e59829aa127b53381ccb86e8ed"
 BOT_TOKEN    = "5909913037:AAGvPpY2Y5V8BkeFmlC1vAF-QofETbrkhhs"
 SESSION      = "BQAcMulgaIjNdPkhB4wQ78Q_62ziMfThq79ikx2npa6fBgZsQFd_fOek6KxbWY3b7Xom1No35F1ioz05t1URzRsJe17JAgc-G-g9r20XS5XN_6vwqn1NeyofSvfTq3v1lERLtGe6mGTfXJFa-KvyetC4jxohx_kUjOkMdQ3PwjRYH9N-05L9m0Wop90TbeK15b-FfoSrmWrEOjTuFWHAfYOR99kJlXKn2RsHS5m4YJbigGRuovsnadtEEyJjBciyjfxgd8DTBkscV5cTxnMXCS1QBlb4RG0j1NkutjQjHe6S60hCidFLWRobQQVCr5sb5NO8QKaa7x4Jxcy6Z6_u9PgTAAAAATRtdQwA"
 DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
