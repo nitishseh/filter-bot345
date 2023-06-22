4f4dd19fce0b75dc38dfc304c7d3b7ec62aba8aa
nitishseh <136986361+nitishseh@users.noreply.github.com>
2023-06-22 21:44:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 26374756
 API_HASH     = "2e3be9e59829aa127b53381ccb86e8ed"
-BOT_TOKEN    = "5909913037:AAGvPpY2Y5V8BkeFmlC1vAF-QofETbrkhhs"
+BOT_TOKEN    = "6275274205:AAGFRa76qLI2D4hBnCrfKw83qI6NW84OLIQ"
 SESSION      = "BQAcMulgaIjNdPkhB4wQ78Q_62ziMfThq79ikx2npa6fBgZsQFd_fOek6KxbWY3b7Xom1No35F1ioz05t1URzRsJe17JAgc-G-g9r20XS5XN_6vwqn1NeyofSvfTq3v1lERLtGe6mGTfXJFa-KvyetC4jxohx_kUjOkMdQ3PwjRYH9N-05L9m0Wop90TbeK15b-FfoSrmWrEOjTuFWHAfYOR99kJlXKn2RsHS5m4YJbigGRuovsnadtEEyJjBciyjfxgd8DTBkscV5cTxnMXCS1QBlb4RG0j1NkutjQjHe6S60hCidFLWRobQQVCr5sb5NO8QKaa7x4Jxcy6Z6_u9PgTAAAAATRtdQwA"
-DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://nitish:nitish@cluster0.g4kggzb.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001909483981
 ADMIN        = 1314385986
 CHANNEL      = "@CyNiTeBaCkUp"
